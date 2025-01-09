7ab0a5743d03c59151bc7385756117a192e3175f
thtamagnau <thomas.tamagnaud@etu.uca.fr>
2025-01-09 13:31:07 +0100

    factorielle en python

--- python/factorielle.py
+++ python/factorielle.py
@@ -1,5 +1,22 @@
 def factorielle_recursive(n : int) -> int:
+    match n:
+        case 0 | 1: return 1 
+        case _: return n * factorielle_recursive(n-1)
+    
+
+def factorielle_recursive_avec_si(n : int) -> int:
     if (n == 0 or n == 1):
         return 1
-    return n * factorielle_recursive(n-1)
+    return n * factorielle_recursive_avec_si(n-1)
 
+def factorielle_iterative(n : int) -> int:
+    resultat = 1
+    while n != 0:
+        resultat *= n
+        n -= 1
+    return resultat
+
+
+for i in range(10):
+    print(f"factorielle({i: >2}) = {factorielle_recursive(i): >7} (recursif) {factorielle_iterative(i): <7} (iteratif)")
+print()
