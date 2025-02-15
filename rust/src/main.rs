de884893d8891d8a765b2da19e75f38999b61505
thtamagnau <thomas.tamagnaud@etu.uca.fr>
2025-02-15 07:57:43 +0100

    small change

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
+#![allow(unused_imports)]
 
 mod code_cesar;
 use code_cesar::*;
@@ -14,10 +15,10 @@ use composition::*;
 
 fn main() 
 {
-    for _ in 0..30 { println!(); }
+    for _ in 0..10 { println!(); }
 
-    somme_entier_exemple();
+    //somme_entier_exemple();
     factorielle_exemple();
-    code_cesar_exemple();
-    composition_exemple();
+    //code_cesar_exemple();
+    //composition_exemple();
 }
